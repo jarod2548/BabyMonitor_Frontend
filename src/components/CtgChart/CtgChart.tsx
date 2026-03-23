bfaf3c31534460ddd61edb1cc55b1fcd4d5f4509
Alexander <alexander@Alexanders-MacBook-Air.local>
2026-03-23 10:10:31 +0100

    grafieken uit elkaar gesplits

--- src/components/CtgChart/CtgChart.tsx
+++ src/components/CtgChart/CtgChart.tsx
@@ -7,20 +7,28 @@
 
   const formatTimestamp = (timestamp: string) =>new Date(timestamp).toLocaleTimeString([], {hour: "2-digit",minute: "2-digit",});
 
-  function CtgChart({data = mockCtgData,isAnimationActive = true,}: 
-    
-    CtgChartProps) {
+  function CtgChart({data = mockCtgData,isAnimationActive = true,}: CtgChartProps) {
     return (
     <div className="ctg-chart">
+      {/* Bovenste Chart: foetale hartslag */}
       <ResponsiveContainer>
         <LineChart data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="timestamp" minTickGap={24} tickFormatter={formatTimestamp} />
           <YAxis yAxisId="heartRate" width={50} domain={[90, 190]} label={{ value: "BPM", angle: -90, position: "insideLeft" }} />
-          <YAxis yAxisId="toco" orientation="right" width={40} domain={[0, 100]} label={{ value: "TOCO", angle: 90, position: "insideRight" }} />
           <Tooltip labelFormatter={(value) => formatTimestamp(String(value))} />
           <Legend />
           <Line yAxisId="heartRate" type="monotone" dataKey="fhrBpm" name="Foetal heart rate" stroke="#cb4335" dot={false} strokeWidth={2} isAnimationActive={isAnimationActive} />
+        </LineChart>
+      </ResponsiveContainer>
+      <ResponsiveContainer>
+        <LineChart data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="timestamp" minTickGap={24} tickFormatter={formatTimestamp} />
+          <YAxis yAxisId="toco" width={50} domain={[90, 190]} label={{ value: "TOCO", angle: -90, position: "insideLeft" }} />
+          <YAxis yAxisId="toco" width={40} domain={[90, 190]} label={{ value: "TOCO", angle: -90, position: "insideLeft" }} />
+          <Tooltip labelFormatter={(value) => formatTimestamp(String(value))} />
+          <Legend />
           <Line yAxisId="toco" type="monotone" dataKey="toco" name="Contractions" stroke="#2874a6" dot={false} strokeWidth={2} isAnimationActive={isAnimationActive} />
         </LineChart>
       </ResponsiveContainer>
