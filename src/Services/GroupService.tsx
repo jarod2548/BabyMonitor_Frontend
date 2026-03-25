0e3a1418c5f10c3d552933395a9491b8f5244120
jarod2548 <jarodkonijnenberg@gmail.com>
2026-03-25 09:23:04 +0100

    main update

--- src/Services/GroupService.tsx
+++ src/Services/GroupService.tsx
@@ -1,15 +1,20 @@
 import axios from "axios";
 import type { Group } from "../contracts/Group";
+import type { maakGroepRequest } from "../contracts/maakGroepRequest";
 
 export class GroupService {
 
      fakeGroepen : Group[] = [
-        { groepId: "1", groepNaam: "Groep A" },
-        { groepId: "2", groepNaam: "Groep B" },
+        { groepId: "1", naam: "Groep A" },
+        { groepId: "2", naam: "Groep B" },
+        { groepId: "1", naam: "Groep A" },
+        { groepId: "2", naam: "Groep B" },
     ];
 
-    async createGroup(groepNaam: string) {
-        const response = await axios.post("/api/createGroup", groepNaam);
+    async createGroup(groepRequest : maakGroepRequest) {
+        console.log(groepRequest);
+        const response = await axios.post("/api/groepen", groepRequest);
+        console.log(response);
         return response;
     }
     
@@ -19,8 +24,12 @@ export class GroupService {
     async getFakeGroups(){
         return await this.fakeGroepen;
     }
-    leaveGroup(groepId : string){
-        return axios.delete(`/api/leaveGroup/${groepId}`);
+    leaveGroupOnUnload() {
+        const id = localStorage.getItem("groupId");
+        if (!id) return;
+
+        const url = `/api/leaveGroup/${id}`;
+        navigator.sendBeacon(url);
     }
 
     joinGroup(groepId : string){
--- src/pages/Home/App.tsx
+++ src/pages/Home/App.tsx
@@ -6,6 +6,9 @@ import { groupService } from "../../Services/GroupService";
 import { useState } from "react";
 import './App.css';
 import type { Group } from "../../contracts/Group";
+import { CreateGroupModal } from "./CreateGroupModal";
+import { JoinGroupModal } from "./JoinGroupModal";
+import type { maakGroepRequest } from "../../contracts/maakGroepRequest";
 
 function Home() {
   const navigate = useNavigate();
@@ -22,29 +25,34 @@ function Home() {
 
   const fetchGroepen = async () => {
     try {
-      const response = await groupService.getFakeGroups();
-      setGroepen(response);
+      const response = await groupService.getGroups();
+      setGroepen(response.data);
     } catch (error) {
       console.log(error);
     }
   }
 
-  const goToTeacher = async () => {
+  const goToTeacher = async (GroepNaam : string) => {
     try{
       await wsService.connect();
-      const groupResponse = await groupService.createGroup("groep");
-      localStorage.setItem("groupId", groupResponse.data);
+      const groupRequest : maakGroepRequest = {
+          naam: GroepNaam
+      };
+      const groupResponse = await groupService.createGroup(groupRequest);
+      localStorage.setItem("groupId", groupResponse.data.id);
+      localStorage.setItem("role", "teacher");
       navigate("/teacher");
     }
     catch(error){
       console.log(error);
     }
   };
-  const goToTeacherDev = () => {
-      navigate("/teacher");
-  };
+  //const goToTeacherDev = () => {
+  //    navigate("/teacher");
+  //};
   const goToStudent = (groepId : string) => {
     console.log("Clicked group:", groepId);
+    localStorage.setItem("role", "student");
       navigate("/student");
   };
 
@@ -53,55 +61,22 @@ function Home() {
   return (
     <div>
       <h1>Welcome to Baby Monitor</h1>
-      <button onClick={goToTeacher}>Leraar</button>
-      <button onClick={goToTeacherDev}>Leraar DEVOLEPMENT</button>
-      <button
-        style={{ position: "absolute", top: 20, right: 20, zIndex: 1000 }}
-        onClick={showCreateGroup}
-      >
-      </button>
-      <button
-        style={{ position: "absolute", top: 80, right: 20, zIndex: 1000 }}
-        onClick={showJoinGroup}
-      >
-      </button>
+      <div className="button-group">
+        <button onClick={showCreateGroup}>Start een groep als docent</button>
+        <button onClick={showJoinGroup}>Doe mee als student</button>
+      </div>
 
-                {creationVisible && (
-            <div className="overlay">
-              <div className="modal-box">
-                <h1>Voer een groeps naam in</h1>
-                <input type="text" placeholder="groepnaam" autoFocus required/>
-                <button>Start als leraar</button>
-                <button 
-                  onClick={showCreateGroup}
-                >Ga terug
-                </button>
-              </div>
-            </div>
-          )}
-          {joinVisible && (
-            <div className="overlay">
-              <div className="modal-box">
-                <h1>Doe mee met een groep</h1>
-                <div>
-                  {groepen.map(g => (
-                    <button key={g.groepId} onClick={() => goToStudent(g.groepId)}>
-                      {g.groepNaam}
-                    </button>
-                  ))}
-                </div>
-                <button 
-                  onClick={showJoinGroup}
-                >Ga terug
-                </button>
-              </div>
-            </div>
-          )}
+      {creationVisible && <CreateGroupModal onClose={showCreateGroup} onCreateGroup={goToTeacher}/>}
+      {joinVisible && (
+        <JoinGroupModal
+          groepen={groepen}
+          onSelectGroup={goToStudent}
+          onClose={showJoinGroup}
+        />
+      )}
     </div>
   );
-
-}
-
+};
 function App() {
   return (
     <Routes>
