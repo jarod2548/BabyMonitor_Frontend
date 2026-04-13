3160a4ef6f170491fc09cc9621a77e99955fad3d
Alexander <alexander@Alexanders-MacBook-Air.local>
2026-04-13 11:18:06 +0200

    Home pagina css aangepast en refactor van app.tsx

--- src/pages/Home/App.tsx
+++ src/pages/Home/App.tsx
@@ -1,111 +1,20 @@
-import {  Route, Routes, useNavigate } from "react-router-dom";
-import { wsService } from "../../WebSocketService";
+import { Route, Routes } from "react-router-dom";
 import Dashboard from "../Dashboard_Docent/Dashboard_Docent";
 import Student from "../Dashboard_Student/Dashboard_Student";
-import { groupService } from "../../Services/GroupService";
-import { useState } from "react";
-import './App.css';
-import type { Group } from "../../contracts/Group";
-import { CreateGroupModal } from "./CreateGroupModal";
-import { JoinGroupModal } from "./JoinGroupModal";
-import type { maakGroepRequest } from "../../contracts/maakGroepRequest";
-import type { HeartbeatData } from "../../contracts/HeartbeatData";
-import { heartbeatService } from "../../Services/HeartbeatService";
 import Login from "../Login/Login";
-import { useAuth } from "./useAuth";
+import Home from "./Home";
+import Home_Docent from "./Home_Docent";
 
-function Home() {
-  const navigate = useNavigate();
-  const loggedIn = useAuth();
-
-  const [creationVisible, setCreationVisible] = useState(false);
-  const [joinVisible, setJoinVisible] = useState(false);
-  const [groepen, setGroepen] = useState<Group[]>([])
-
-  const showCreateGroup = () => setCreationVisible(prev => !prev);
-  const showJoinGroup = async () => {
-    await fetchGroepen();
-    setJoinVisible(prev => !prev);
-  }
-
-  const fetchGroepen = async () => {
-    try {
-      const response = await groupService.getFakeGroups();
-      setGroepen(response);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  const goToTeacher = async (GroepNaam : string) => {
-    try{
-      await wsService.connect();
-      const groupRequest : maakGroepRequest = {
-          naam: GroepNaam
-      };
-      const groupResponse = await groupService.createGroup(groupRequest);
-      localStorage.setItem("groupId", groupResponse.data.id);
-      wsService.subscribe<HeartbeatData>(`/heartbeat/${groupResponse.data.id}`, heartbeatReceived);
-      navigate("/teacher");
-    }
-    catch(error){
-      console.log(error);
-    }
-  };
-//
-  //const goToTeacherDev = () =>{
-//
-  //    navigate("/teacher");
-  //};
-  
-  const heartbeatReceived = (data : HeartbeatData) => {
-    heartbeatService.heartbeatReceived(data);
-  }
-
-  const goToStudent = (groepId : string) => {
-    console.log("Clicked group:", groepId);
-    wsService.subscribe<HeartbeatData>(`/topic/heartbeat/${groepId}`, heartbeatReceived);
-      navigate("/student");
-  };
-
-  const goToLogin = () => {
-    navigate("/login")
-  }
-
-
-
-  return (
-    <div>
-      <h1></h1>
-      <div className="button-group">
-        <button onClick={showCreateGroup}>Start een groep als docent</button>
-        <button onClick={showJoinGroup}>Doe mee als student</button>
-        {loggedIn ? <p>Welcome</p> : <button onClick={goToLogin}>Login</button>
-        }
-      </div>
-
-      {creationVisible && <CreateGroupModal onClose={showCreateGroup} onCreateGroup={goToTeacher}/>}
-      {joinVisible && (
-        <JoinGroupModal
-          groepen={groepen}
-          onSelectGroup={goToStudent}
-          onClose={showJoinGroup}
-        />
-      )}
-    </div>
-  );
-};
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/home_docent" element={<Home_Docent />} />
       <Route path="/teacher" element={<Dashboard />} />
       <Route path="/student" element={<Student />} />
-      <Route path="/login" element={<Login />}/>
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
 
 export default App;
-
-
